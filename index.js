bb4fe2df4ed958ebfa1cb161e84775f972673bea
Rafael Lisboa <rafael.lisboa0@gmail.com>
2023-05-10 23:52:50 +0000

    new

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const path = require('path');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
